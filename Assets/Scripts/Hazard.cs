729271e947ec46d8f5eecaa8f846fc877c61b099
Rory <rory.lacayon@gmail.com>
2020-12-01 16:58:57 -0500

    C.13 - Built game Hazards

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour 
+{
+	// These will hold some references to assets that will be used for when the player touches the saw blade and triggers the hazard action.
+	public GameObject playerDeathPrefab;
+	public AudioClip deathClip;
+	public Sprite hitSprite;
+	private SpriteRenderer spriteRenderer;
+
+	// This caches a reference to the SpriteRenderer when the script starts.
+	void Awake()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		// 1 First we check to ensure the colliding GameObject has the "Player" tag.
+		if (coll.transform.tag == "Player")
+		{
+			// 2 Next, determine if an AudioClip has been assigned to the script and that a valid Audio Source component exists. If so, play the deathClip audio effect.
+			var audioSource = GetComponent<AudioSource>();
+			if (audioSource != null && deathClip != null)
+			{
+				audioSource.PlayOneShot(deathClip);
+			}
+			// 3 Instantiate the playerDeathPrefab at the collision point and swap the sprite of the saw blade with the hitSprite version.
+			Instantiate(playerDeathPrefab, coll.contacts[0].point,
+			Quaternion.identity);
+			spriteRenderer.sprite = hitSprite;
+			// 4 Lastly, destroy the colliding object (the player).
+			Destroy(coll.gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/RotationScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+	public float rotationsPerMinute = 640f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//This will rotate the GameObject the script is attached to around the Z-axis based on the value of rotationsPerMinute.
+		transform.Rotate(0, 0, rotationsPerMinute * Time.deltaTime, Space.Self);
+	}
+}
