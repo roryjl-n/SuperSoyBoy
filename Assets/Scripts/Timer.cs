a2e72ab26c2972291d1638a9b888dd6bbd21010b
Rory <rory.lacayon@gmail.com>
2020-12-08 16:29:39 -0500

    C.13 - Added a level timer and Set everything up to build my own game level (NotSetToAnInstanceOfanObject FIXED)

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour 
+{
+	/*  This code will grab and cache a reference to the Text component on the same
+	GameObject the script exists on. In the Update() loop, the text will be changed to
+	display the time since the level last reloaded, rounded to two decimal places.*/
+
+    private Text timerText;
+
+	void Awake()
+	{
+		timerText = GetComponent<Text>();
+	}
+
+	void Update()
+	{
+		timerText.text =
+		System.Math.Round((decimal)Time.timeSinceLevelLoad,
+		2).ToString();
+	}
+}
