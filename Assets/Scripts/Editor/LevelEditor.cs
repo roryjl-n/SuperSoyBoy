01b8b765748e0dac95851f22dce45e0dbb1de207
Rory <rory.lacayon@gmail.com>
2020-12-15 17:59:29 -0500

    C.14 - Built a custom editor for saving levels

---
+++ Assets/Scripts/Editor/LevelEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+[CustomEditor(typeof(Level))]
+public class LevelEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Save level"))
+        {
+
+        }
+    }
+}
